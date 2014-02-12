77b783d05800afd4584b6a325274a29774b64cc6
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2014-02-12 21:06:09 +0400

    Task2

--- seminar120214_2.R
+++ seminar120214_2.R
@@ -1,5 +1,5 @@
 sizes <- c(10, 50, 100, 1000)
-samples <- list(rep(NA, 1000), length(sizes))
+samples <- matrix(NA, length(sizes), 1000)
 
 for (i in 1:length(sizes)) {
     sample <- rep(NA, 1000)
@@ -7,5 +7,9 @@ for (i in 1:length(sizes)) {
       sample[j] <- mean(rnorm(sizes[i], 10, 5))
     }
     print(sprintf("Sampling size: %d Mean: %f, sd: %f, cvar: %f", sizes[i], mean(sample), sd(sample), sd(sample)/mean(sample)))
-    plot(hist(sample), col = "green")
+    plot(hist(sample), col = "green", main=sprintf("Histogram for sample sizes: %d", sizes[i]))
+    samples[i,] = sample
 }
+
+
+
