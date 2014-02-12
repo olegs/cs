0b196a26f5ff28ce1a1f4a1d6179b2d1ee4e2c9a
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2014-02-12 21:19:15 +0400

    Task3

---
+++ seminar12022014_3.R
@@ -0,0 +1,31 @@
+require(moments)
+
+analyze <- function(sample) {
+  n <- length(sample)
+  print(sprintf("Mean: %f, min: %f, max: %f, median: %f, sd: %f cvar: %f", 
+                mean(sample), min(sample), max(sample), median(sample), 
+                sd(sample),
+                sd(sample)/mean(sample)
+  )
+  )
+  
+}
+
+norm_sample1 <- rnorm(80, mean = 7, sd = 1)
+norm_sample2 <- rnorm(60, mean = 8, sd = 2)
+print("Analyzing normal distribution sampling1")
+analyze(norm_sample1)
+print("Analyzing normal distribution sampling2")
+analyze(norm_sample2)
+
+sum <- append(norm_sample1, norm_sample2)
+print("Analyzing sum")
+analyze(sum)
+
+h1 <- hist(sum, col="green")
+lines(h1$counts ~ h1$mids, col="red")
+
+rug(sum)
+str(sum)
+h1$density
+c(0, cumsum(frequency(sum)))
