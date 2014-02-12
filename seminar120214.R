d9f4042f6d2b455476a0f454cf03fa378bed2c71
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2014-02-12 20:22:59 +0400

    Seminar 12.02.2014 commit

---
+++ seminar120214.R
@@ -0,0 +1,31 @@
+require(moments)
+
+analyze <- function(sample) {
+  n <- length(sample)
+  print(sprintf("Mean: %f, min: %f, max: %f, median: %f, range: %f, quantile1: %f, quantile3: %f, 
+                var: %f, fixed var: %f, sd: %f cvar: %f, skewness: %f, kurtosis: %f", 
+                mean(sample), min(sample), median(sample), max(sample), range(sample), 
+                quantile(sample, .25), 
+                quantile(sample, .75),
+                var(sample),
+                n/(n-1)*var(sample),
+                sd(sample),
+                sd(sample)/mean(sample),
+                skewness(sample),
+                kurtosis(sample)
+                )
+        )
+  
+}
+
+print("Analyzing normal distribution sampling")
+norm_sample <- rnorm(100, mean = 0, sd = 1)
+analyze(norm_sample)
+
+print("Analyzing exponention distribution sampling")
+exp_sample <- rexp(100, rate=0.5)
+analyze(exp_sample)
+
+hist(norm_sample, exp_sample, main = "Histogram for normal and exponential", breaks=10, col="pink")
+boxplot(norm_sample, exp_sample, main = "Boxplot for normal and exponential", names = c("Normal", "Exp"))
+
